3b5bda4f15a9acfcefde016cdfce1124af351037
Javier <97004111+jcarrozadev@users.noreply.github.com>
2024-09-20 09:33:52 +0200

    DWES
    Carpeta de Desarrollo Web Entorno Desarrollo

---
+++ DWES/factorialPrueba/prueba.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset=UTF-8>
+        <meta name=viewport content='width=device-width, initial-scale=1.0'>
+        <meta name=author content='Javier Arias Carroza jariasc14@gmail.com'>
+        <link rel=icon type=image/x-icon href=/img/favicon.png>
+        <title>DWES | Prueba 1</title>
+        
+        <link rel=stylesheet href=css/reset.css>
+        <!-- Resto de hojas de estilo CSS -->
+    </head>
+    <body>
+        <?php $conjunto = 'PHP'; ?>
+
+        <h1><?php echo 'Página HTML con '.$conjunto.''; ?></h1>
+
+        <?php echo PHP_VERSION; ?>
+        <?php echo PHP_OS; ?>
+        <!-- Scripts de JS al final del body -->
+    </body>
+</html>
