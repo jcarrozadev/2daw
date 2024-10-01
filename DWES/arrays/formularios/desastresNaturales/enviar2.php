ae459af590f9f1ce1fc8013cdd12e68f57ea8212
Javier <97004111+jcarrozadev@users.noreply.github.com>
2024-10-01 10:52:49 +0200

    DWES

---
+++ DWES/arrays/formularios/desastresNaturales/enviar2.php
@@ -0,0 +1,10 @@
+<?php
+
+    print_r($_POST);
+    echo '<br/><br/>';
+    var_dump($_POST);
+    echo '<br/><br/><br/>';
+    print_r($_POST['notificaciones']);
+    echo '<br/><br/>';
+    var_dump($_POST['notificaciones']);
+?>
