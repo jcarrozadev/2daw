8c5a5a648dbed8f181b04a315ce4dd6f0ab3e259
Javier <97004111+jcarrozadev@users.noreply.github.com>
2024-10-04 09:15:13 +0200

    Location v1.1

---
+++ DWES/location/calcular.php
@@ -0,0 +1,24 @@
+<?php
+
+    $num = $_GET['numero']; // Recogemos el número introducido
+    $cuadrado = $num * $num; // Calculamos el cuadrado
+
+    /**
+     * Función para calcular si es par o impar, si el resto es igual a 0 devolverá verdadero (par) y si no devolverá falso (impar)
+     */
+    function calcularParImpar($num) {
+        if ($num % 2 == 0) {
+            return true;
+        } else {
+            return false;
+            
+        }
+    }
+
+    // Llamamos y comprobamos si es par o impar y redirige según el caso
+    if (calcularParImpar($num)) 
+        header("Location:par.php?numero=$num&cuadr=$cuadrado");
+    else 
+        header("Location:impar.php?numero=$num&cuadr=$cuadrado");
+
+?>
---
+++ DWES/location/par.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>¡Numero Par!</title>
+        <style>
+            body {
+                text-align:center;
+                background-color: lightblue;
+            }
+            table {
+                margin: 0 auto;
+                width: 50%;
+                border-collapse: collapse;
+            }
+            th {
+                font-size: 1.5em;
+                color: white;
+                padding: 2%;
+            }
+            td{
+                font-size: 1.2em;
+                text-align:center;
+            }
+            tr,td {
+                margin: 1%;
+                padding:1%;
+            }
+            tr,th,td {
+                border: 1px solid black;
+                padding:5%;
+            }
+        </style>
+    </head>
+    <body>
+        <h1>¡El número es par!</h1>
+        <table>
+            <tr>
+                <th>
+                    Número: 
+                </th>
+                <th>
+                    Cuadrado:
+                </th>
+                <th>
+                    Paridad:
+                </th>
+            </tr>
+            <tr>
+                <?php
+                    echo '
+                        <td>
+                            '.$_GET['numero'].'
+                        </td>
+                        <td>
+                            '.$_GET['cuadr'].'
+                        </td>
+                        <td>
+                            Par
+                        </td>
+                    ';
+                ?>
+            </tr>
+        </table>
+    </body>
+</html>
